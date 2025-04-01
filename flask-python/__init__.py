886831591ca2a19de93eebc6054de073e55b825f
spookeyy <pangasmeshack@gmail.com>
2025-04-01 23:38:02 +0300

    app configurations setup

--- flask-python/__init__.py
+++ flask-python/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import Config
 
-def create_app():
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_class=Config):
 
     app = (Flask(__name__))
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+
+    from .models.users import User
 
     from .routes.users import users
     app.register_blueprint(users)
